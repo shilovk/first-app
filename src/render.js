a757796e7200cd182acbd81ce62489f92d656c0b
Konstantin Shilov <shilovk@gmail.com>
2022-09-08 20:48:17 +0300

    server render ProductCard

---
+++ src/render.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+
+import App from "./App";
+
+const product = {
+	'Title': 'Apple Macbook Pro 16\'',
+	'Cover': 'https://micro-line.ru/images/thumbnails/340/340/detailed/700/952594.jpg',
+	'Price': '2800',
+	'Description': 'For work and fun.',
+	'Discount': true
+}
+
+const render = () => ReactDOMServer.renderToString(<App product={product} />);
+
+export default render;
